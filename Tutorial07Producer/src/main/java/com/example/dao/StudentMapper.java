f9963bbf13cdf2d12ac78aafba89cd4a807548a1
fildzalarissa <fildzalarissa@gamil.com>
2017-11-04 13:17:15 +0700

    perbaikan mapper latihan 1 dan 2

--- Tutorial07Producer/src/main/java/com/example/dao/StudentMapper.java
+++ Tutorial07Producer/src/main/java/com/example/dao/StudentMapper.java
@@ -51,6 +51,9 @@ public interface StudentMapper
     		"from studentcourse join course " +
     		"on studentcourse.id_course = course.id_course " +
     		"where studentcourse.npm = #{npm}")
+    @Results(value = {
+			@Result(property="idCourse", column="id_course")
+    })
     List<CourseModel> selectCourses (@Param("npm") String npm);
 
 	
